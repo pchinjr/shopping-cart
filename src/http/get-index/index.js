eacf3e765b3865f3fc8d4f75a538d8fcf4be20ec
Paul Chin Jr <pchinjr@gmail.com>
2021-03-31 09:37:26 -0400

    input type number

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -19,7 +19,7 @@ async function http (req) {
     <ul>
     <li>Item 1</li>
     <form action='cart' method='POST'>
-    <input type="text" name="quantity"/>
+    <input type="number" name="quantity"/>
     <input type="hidden" name="productId" value='001'>
     <button>Add to Cart</button>
     </form>
@@ -29,7 +29,7 @@ async function http (req) {
     </form>
     <li>Item 2</li>
     <form action='cart' method='POST'>
-    <input type="text" name="quantity"/>
+    <input type="number" name="quantity"/>
     <input type="hidden" name="productId" value='002'>
     <button>Add to Cart</button>
     </form>
@@ -39,7 +39,7 @@ async function http (req) {
     </form>
     <li>Item 3</li>
     <form action='cart' method='POST'>
-    <input type="text" name="quantity"/>
+    <input type="number" name="quantity"/>
     <input type="hidden" name="productId" value='003'>
     <button>Add to Cart</button>
     </form>
